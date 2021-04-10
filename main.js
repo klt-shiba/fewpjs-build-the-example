6011b5612010520bf7ffada51dcff528d9392a60
Colin Tseng <klt.aus@yahoo.com>
2021-04-10 16:45:15 +1000

    Completed assignment

--- main.js
+++ main.js
@@ -1,11 +1,50 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+// Hide modal on load
+let modal = document.getElementById("modal"); 
+modal.classList.add("hidden"); 
 
-// Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", function() {
+
+  const emptyHeart = '♡'
+  const fullHeart = '♥'
+  const heartButton = document.getElementsByClassName("like-glyph");
+  
+  // Your JavaScript code goes here!
+  const heartColour = {
+    "red": "",
+    "": "red"
+  }
+  const heartFill = {
+    "♡": "♥",
+    "♥": "♡"
+  }
 
 
+  
+let likeArticle = (e) => {
+// e.target // the element that triggered the event
+  let heart = e.target
+  console.log(heart);
+  //URL for the API/Endpoint 
+  mimicServerCall("") //fetch()
+  // if the call succeeds, the promise 
+  .then(function(serverMessage) {
+    heart.innerText = heartFill[heart.innerText]
+    heart.style.color = heartColour[heart.style.color]
+  })
+  .catch((e) => {
+    modal.classList.remove("hidden"); 
+    modal.innerHTML = `${e}`
+    setTimeout(function(){
+      modal.classList.add("hidden");  
+    }, 5000);
+  })
+};
 
+for (let glyph of heartButton) {
+  glyph.addEventListener("click", likeArticle)
+}
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
